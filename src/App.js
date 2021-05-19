6e0769a7a95c16884ad25cea71c2be13ebd90206
Alexander Ditz <77445887+AD2348f@users.noreply.github.com>
2021-05-19 10:48:15 +0200

    Co-authored-by: Darth-Balls <Darth-Balls@users.noreply.github.com> Co-authored-by: Baikalworld <Baikalworld@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App__Header">        
+        <h3>header</h3>
+        <img></img>
+        <nav>
+          <ul className="App__Navigation">
+            <li>Für Arbeitgeber</li>
+            <li>Vorgemerkt</li>
+            <li>Mein Profil</li>
+          </ul>
+        </nav>
       </header>
+      <main className="App__Main">
+        {/* Sidebar links */}
+        <div className="App__Sidebar-links">
+          <div>Seiten Feld 1</div>
+          <div>Seiten Feld 2</div>
+          <div>Seiten Feld 3</div>
+        </div>
+        {/* Suchfeld */}
+        <div className="App__Mittelteil">
+          <div className="App__Suchfelder-wrapper">
+          <form className="App__Searchfield" action="">
+            <input type="text"></input>
+            <input type="text"></input>
+            <button>Suchen</button>
+          </form>
+          </div>
+          
+          {/* Stellenangebote */}
+          <div className="App__Stellenangebote">
+          <div>Beispiel Feld 1</div>
+          <div>Beispiel Feld 2</div>
+          <div>Beispiel Feld 3</div>
+          </div>
+        </div>
+      </main>
+      <footer className="App__Footer">
+        <div>Über uns</div>
+        <div>AGB / Datenschutz</div>  
+        <div>Social Media</div>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
